09b19d530850ca7ee7eb1804d012033e5d264ec4
nickzaf95 <nicholaszafiriou@gmail.com>
2020-10-27 16:47:27 +0000

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const findAllPermutations = (str) => {
     if (split.length === 1) {
         return split
     } else {
-        
+        let answer = []
+        for (let i = 0; i < split.length; i++) {
+            for (let j = 0; j < str.length; j++) {
+                for (let k = 0; k < str.length; k++) {
+                    let a = str[i]
+                    let b = str[j]
+                    let c = str[j]
+                    if (a === b || a === c || b === c) {
+                    } else {
+                        answer.push(a.concat(b).concat(c))
+                    }
+                }
+                
+            }
+            
+        }
+        var filteredArray = answer.filter(function(item, pos){
+            return answer.indexOf(item)== pos; 
+        })
+        return filteredArray
     }
 }
